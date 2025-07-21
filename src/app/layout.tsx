83b2aa851f63a5738abaaff0559bd519d64e140f
Prince kyei <prince.kaf12@gmail.com>
2025-07-21 12:33:49 +0200

    fixed3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/logopk.png",
   },
+  viewport: "width=device-width, initial-scale=1", // ✅ critical for mobile
 };
 
 export default function RootLayout({
@@ -29,9 +30,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
